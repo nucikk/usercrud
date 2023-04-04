966daada365d272375d6a3a4f34d6fc2935224f6
nuci <kirianuci@gmail.com>
2023-04-04 21:19:37 +0400

    Add update and delete buttons and update design

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import "../Style/Home.css"
 
 const Home = () => {
   const [data, setData] = useState([]);
-  
+
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users")
       .then(res => setData(res.data))
@@ -15,14 +16,23 @@ const Home = () => {
   }
 
   return (
-    <ul>
-      {data.map((user) => (
-        <li key={user.id} className="user_box">
-          {user.name} {user.surname}
-          {user.email}
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className="container">
+        <ul className="user_list">
+          {data.map((user) => (
+            <li key={user.id} className="user_item">
+             <p className='fullname'>{user.name} </p>  
+             <p className='fullname'>{user.surname} - </p>
+             <p>{user.email}</p>
+             <div className="user_actions">
+             <button className="action_btn_update">Update</button>
+             <button className="action_btn_delete">Delete</button>
+             </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
