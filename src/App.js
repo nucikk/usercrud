e5b7ab5f6c845bd6039d15d65d93cfc550a6ec1e
nuci <kirianuci@gmail.com>
2023-04-05 21:05:06 +0400

    Add button to create a new JSON server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Page/Home';
+import Create from './Page/Create';
 
 function App() {
   return (
     <BrowserRouter>
      <Routes>
        <Route path="/" element={<Home />}></Route>
+       <Route path="/create" element={<Create />}/>
      </Routes>
     </BrowserRouter>
   );
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import "../Style/Home.css"
 
 const Home = () => {
@@ -18,19 +19,27 @@ const Home = () => {
   return (
     <>
       <div className="container">
-        <ul className="user_list">
-          {data.map((user) => (
-            <li key={user.id} className="user_item">
-             <p className='fullname'>{user.name} </p>  
-             <p className='fullname'>{user.surname} - </p>
-             <p>{user.email}</p>
-             <div className="user_actions">
-             <button className="action_btn_update">Update</button>
-             <button className="action_btn_delete">Delete</button>
-             </div>
-            </li>
-          ))}
-        </ul>
+        <div className="user_box">
+
+          <h2 className='crud_title'>Crud App with JSON Server</h2>
+          <Link to="/create">
+            <button className="btn_create">Create +</button>
+          </Link>
+
+          <ul>
+            {data.map((user) => (
+              <li key={user.id} className="user_item">
+                <p className='fullname'>{user.name} </p>
+                <p className='fullname'>{user.surname} - </p>
+                <p>{user.email}</p>
+                <div className="user_actions">
+                  <button className="action_btn_update">Update</button>
+                  <button className="action_btn_delete">Delete</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
