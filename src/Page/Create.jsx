05ef7cba84a5abaf62f7bc798cd3dc93664464d6
nuci <kirianuci@gmail.com>
2023-04-06 11:33:58 +0400

    add create page design for JSON server inputs

--- src/Page/Create.jsx
+++ src/Page/Create.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import "../Style/create.css"
 const Create = () => {
   return (
-    <div>
-      create
+    <div className="form_container">
+     <div className="form_field">
+      <form>
+      <label htmlFor="Full Name">Name</label>
+      <input type="text" name="Full Name" className="form_input" placeholder="Enter Your Name..."/>
+
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" className="form_input"  placeholder="Enter Your Email..."/>
+      <button className="submit_btn">Submit</button>
+      </form>
+     </div>
     </div>
   )
 }
